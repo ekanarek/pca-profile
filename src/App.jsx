cfe2722fe66a9eb921d95c76c44d7bb244cdb1ea
Emily Kanarek <emilyakanarek@gmail.com>
2025-02-19 10:28:25 -0500

    Set up React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import './App.css'
+import Navigation from './components/Navigation/Navigation.jsx';
+import Home from './pages/Home/Home.jsx';
+import Interests from './pages/Interests/Interests.jsx';
+import Library from './pages/Library/Library.jsx';
 
 function App() {
   return (
     <>
-      {/* ADD NAVBAR HERE */}
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/interests" element={<Interests />} />
+          <Route path="/library" element={<Library />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/Library/Library.jsx
@@ -0,0 +1,5 @@
+function Library() {
+    
+}
+
+export default Library
